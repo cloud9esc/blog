8d0c3076fd41aa06ba2b3dce4ece39c872e832ad
Minyeong Lim <cloud9esc@gmail.com>
2019-04-15 15:47:23 +0900

    download sass

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className="IndexPage">
+        <h1 className="headline">
+          Foreground One
+        </h1>
+      </div>
+    );
+  };
+};
