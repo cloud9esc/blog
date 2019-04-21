cba3db1403d6d0959e5fc398d053aa3f4537d970
Minyeong Lim <cloud9esc@gmail.com>
2019-04-21 23:55:21 +0900

    Make recents component

---
+++ components/Recents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+import './Recents.scss';
+
+export default function Recents({ posts }) {
+
+  return (
+    <div className="Recents">
+      <div className="row">
+        
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 //import Link from 'next/link';
-
+import { loadPosts } from '../lib/repository';
 import './index.scss';
 import AppHeader from '../components/AppHeader';
 import Nav from '../components/Nav';
+import Recents from '../components/Recents';
 
 export default class IndexPage extends React.Component {
+
+  static async getInitialProps() {
+    return { posts: await loadPosts() };
+  }
   render() {
     return (
       <div className="IndexPage">
@@ -14,6 +19,9 @@ export default class IndexPage extends React.Component {
           <div className="row">
             <div className="col-12 col-md-3">
               <Nav />
+            </div>
+            <div className="col-12 col-md-9">
+              <Recents posts={this.props.posts}/>
             </div>
           </div>
         </div>
