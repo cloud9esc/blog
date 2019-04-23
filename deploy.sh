3d85a1bdade9ce65c5760d9b5c729f134162e460
Minyeong Lim <cloud9esc@gmail.com>
2019-04-23 22:42:21 +0900

    modify rsync to deploy media files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo Deploy to $1;
 
 rsync -vr --exclude-from '.gitignore' --exclude '.git' . cloud9esc@$1:~/app;
+rsync -vr ./static/media cloud9esc@$1:~/app/static;
 
 ssh cloud9esc@$1 "cd ~/app/production && docker-compose up -d --build && docker container prune -f && docker image prune -f"
