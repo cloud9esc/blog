1b688db160f7dcb5112db482d6ea5763ba93f382
Minyeong Lim <cloud9esc@gmail.com>
2019-04-24 14:04:14 +0900

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 
 import './_app.scss';
 
@@ -19,6 +20,15 @@ class MyApp extends App {
 
     return (
       <Container>
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <div className="global_background" />
         <Component {...pageProps} />
       </Container>
